a26eb29875bec790a939e8e9aec30b906c79153e
Twisted7ech <bradcederberg@gmail.com>
2022-09-22 19:47:30 -0600

    working with comments now

--- routes/comments.js
+++ routes/comments.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const router = express.Router();
-const commentsController = require("../controllers/comments");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const express = require('express')
+const router = express.Router()
+const commentController = require('../controllers/comments')
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-//Comment Routes - simplified for now
-router.post("/createComment/:id", commentsController.createComment);
+//CommentRoutes - simplified for now
+router.post('/createComment/:id', commentController.createComment)
 
+// router.delete("/deletePost/:id", postsController.deletePost);
 
-module.exports = router;
+module.exports = router
